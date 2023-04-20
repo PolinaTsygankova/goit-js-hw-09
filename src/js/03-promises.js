92af1e1dbb0dfdf80cd5a25432e634ec6b38cd86
PolinaTsugankova <pp6056512@gmail.com>
2023-04-20 22:54:47 +0300

    add full task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,7 @@
 //* Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount. Під час кожного виклику передай їй номер промісу (position), що створюється, і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).
 // Доповни код функції createPromise таким чином, щоб вона повертала один проміс, який виконується або відхиляється через delay часу. Значенням промісу повинен бути об'єкт, в якому будуть властивості position і delay зі значеннями однойменних параметрів. Використовуй початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.
+import Notiflix from 'notiflix';
+
 refs = {
   form: document.querySelector('.form'),
   submitBtn: document.querySelector('button[type]'),
@@ -8,41 +10,44 @@ refs = {
   amountInp: document.querySelector('input[name="amount"]'),
 };
 
+let finalDelay = 0;
+
 function onSubmitBtn(e) {
   e.preventDefault();
   const delayInpValue = Number(refs.delayInp.value);
   const stepInpValue = Number(refs.stepInp.value);
   const amountInpValue = Number(refs.amountInp.value);
-  for (let i = 1; i < amountInpValue; i += 1) {
-    createPromise(i, delayInpValue);
+  for (let i = 1; i <= amountInpValue; i += 1) {
+    if (i === 1) {
+      finalDelay += delayInpValue;
+      createPromise(i, finalDelay);
+    } else {
+      finalDelay += stepInpValue;
+      createPromise(i, finalDelay);
+    }
   }
+  finalDelay = 0;
 }
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  console.log(shouldResolve);
+  // console.log(shouldResolve);
 
   const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (shouldResolve) {
-        console.log(resolve(`✅ Fulfilled promise ${position} in ${delay}ms`));
-        // resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      } else if (!shouldResolve) {
-        reject(`❌ Rejected promise ${position} in ${delay}ms`);
-      }
-    }, 500);
+    if (shouldResolve) {
+      setTimeout(() => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      }, delay);
+    } else {
+      setTimeout(() => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      }, delay);
+    }
   });
 }
 
 refs.form.addEventListener('submit', onSubmitBtn);
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-console.log('message');
-console.log('=)');
